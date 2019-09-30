fc4f1f7b8dd80197bb69208f4f1cc9ff8e4e91fa
Matthew Carter <m@ahungry.com>
2019-09-30 00:23:49 -0400

    Work on building up real db persistence

--- project.clj
+++ project.clj
@@ -3,17 +3,32 @@
   :url "https://github.com/ahungry/com.ahungry.comments"
   :license {:name "AGPLv3"
             :url ""}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [
+                 ;; Lang
+                 [org.clojure/clojure "1.10.0"]
+                 [clojure.java-time "0.3.2"]
+
                  ;; Web related deps
                  [compojure "1.6.1"]
                  [cheshire "5.9.0"]
                  [http-kit "2.3.0"]
                  [ring "1.7.1"]
+
+                 ;; Config
+                 [ahungry/xdg-rc "0.0.4"]
+
+                 ;; Database related
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+
+                 ;; Extras
                  [markdown-clj "1.10.0"]
+
                  ;; Logging related
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.clojure/tools.logging "0.5.0"]
                  ]
   :main ^:skip-aot com.ahungry.comments
   :target-path "target/%s"
+  :repl-options {:init-ns com.ahungry.comments}
   :profiles {:uberjar {:aot :all}})
--- src/com/ahungry/dao.clj
+++ src/com/ahungry/dao.clj
@@ -1,9 +1,36 @@
 (ns com.ahungry.dao
   (:require
    [clojure.tools.logging :as log]
+   [clojure.java.jdbc :as jdbc]
    [markdown.transformers :as mt]
    [markdown.core :as md]
+   [java-time :as t]
+   [xdg-rc.core :as xdg-rc]
    ))
+
+(defn data-dir-db []
+  (str (xdg-rc/get-xdg-data-dir) "/ahungry-determinism.db"))
+
+(def db
+  {:classname "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname (data-dir-db)})
+
+(defn make-db []
+  (jdbc/execute! db ["CREATE TABLE IF NOT EXISTS det (
+identity, input, input_types, output, output_type, date);"]))
+
+(defn time-now []
+  (str (t/local-date-time)))
+
+(defn q
+  "Wrapper for basic querying, with logging incorporated.
+  SS = jdbc interface (vec: sql + args)."
+  [ss]
+  (let [result (apply jdbc/query db [ss])]
+    (log/debug ss)
+    (log/debug result)
+    result))
 
 (defn escape-images [text state]
   [(clojure.string/replace text #"(!\[.*?\]\()(.+?)(\))" "") state])
