73cd0b87dff9346dbe9ccb0e80f777eb8df44262
Matthew Carter <m@ahungry.com>
2020-02-01 13:53:09 -0500

    namespace the iframe code

--- resources/iframe.js
+++ resources/iframe.js
@@ -1,39 +1,42 @@
 // Util
-const ce = x => document.createElement(x)
-const $ = x => document.querySelector(x)
-const $$ = x => document.querySelectorAll(x)
+// Wrap in an IIFE to avoid collision with code outside.
+(function () {
+  const ce = x => document.createElement(x)
+  const $ = x => document.querySelector(x)
+  const $$ = x => document.querySelectorAll(x)
 
-window.addEventListener('message', receiveMessage, false);
+  window.addEventListener('message', receiveMessage, false);
 
-function handler (m) {
-  console.log('handler', { m })
+  function handler (m) {
+    console.log('handler', { m })
 
-  switch (m.type) {
-    case 'getHref':
-      $('#comments-frame').contentWindow.postMessage(
-        {
-          type: 'getHref',
-          val: window.location.href,
-        }, '*')
-      break
+    switch (m.type) {
+      case 'getHref':
+        $('#comments-frame').contentWindow.postMessage(
+          {
+            type: 'getHref',
+            val: window.location.href,
+          }, '*')
+        break
 
-    case 'resize':
-      const { w, h } = m
-      $('#comments-frame').width = w
-      $('#comments-frame').height = h
-      $('#comments-frame').style.width = `${w}px`
-      $('#comments-frame').style.height = `${h}px`
-      $('#comments-frame').style.display = `block`
-      break
+      case 'resize':
+        const { w, h } = m
+        $('#comments-frame').width = w
+        $('#comments-frame').height = h
+        $('#comments-frame').style.width = `${w}px`
+        $('#comments-frame').style.height = `${h}px`
+        $('#comments-frame').style.display = `block`
+        break
+    }
   }
-}
 
-function receiveMessage (event) {
-  // Events that get handled up at the potential iframe source.
-  if (event.origin === 'http://comments.ahungry.com' ||
-    event.origin === 'https://comments.ahungry.com' ||
-    event.origin === 'http://localhost:3001') {
-    const { data } = event
-    handler(data)
+  function receiveMessage (event) {
+    // Events that get handled up at the potential iframe source.
+    if (event.origin === 'http://comments.ahungry.com' ||
+      event.origin === 'https://comments.ahungry.com' ||
+      event.origin === 'http://localhost:3001') {
+      const { data } = event
+      handler(data)
+    }
   }
-}
+})()
